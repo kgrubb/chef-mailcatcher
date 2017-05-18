0b465f94d98e78e341d3ae8af317f8389123e9b0
Volodymyr Babchynskyy <vvchik@gmail.com>
2017-05-18 19:09:18 +0300

    fixed cookbook version to follow semver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'keli.grubb@spinen.com'
 license          'Apache License v2.0'
 description      'Installs/Configures mailcatcher'
 long_description 'Installs/Configures mailcatcher'
-version          '0.2'
+version          '0.2.0'
 issues_url       'https://github.com/kgrubb/chef-mailcatcher/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/kgrubb/chef-mailcatcher' if respond_to?(:source_url)
 chef_version     '>= 12.0' if respond_to?(:chef_version)
