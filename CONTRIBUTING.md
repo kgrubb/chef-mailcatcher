880db7d7c8de60a9ba94816bf4d3db03900ea6dd
Madison Grubb <madison.grubb@itential.com>
2017-05-18 13:46:40 -0400

    cleanup license, metadata, and add a contribution guide

---
+++ CONTRIBUTING.md
@@ -0,0 +1,19 @@
+Contributing Guidelines
+===
+
+## 1\. Install Chefdk on your system
+
+ChefDK is used to work on and test this cookbook. You can find a link to the
+Chef Development Kit [here](https://downloads.chef.io/chefdk).
+
+## 2\. Fork the repository & clone locally
+
+Make changes locally, commit, and push the changes to your fork.
+
+## 3\. Submit a pull request
+
+Contributions follow github's [pull request
+model](https://help.github.com/articles/using-pull-requests/).
+
+Commit your changes and submit a new pull request to the chef-mailcatcher
+`master` branch.
--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,17 @@
 name             'chef-mailcatcher'
 maintainer       'Keli Grubb'
 maintainer_email 'keligrubb324@gmail.com'
-license          'Apache License v2.0'
+license          'Apache 2.0'
 description      'Installs/Configures mailcatcher'
 long_description 'Installs/Configures mailcatcher'
-version          '0.2.0'
+version          '0.2.1'
 issues_url       'https://github.com/kgrubb/chef-mailcatcher/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/kgrubb/chef-mailcatcher' if respond_to?(:source_url)
 chef_version     '>= 12.0' if respond_to?(:chef_version)
 
 recipe 'chef-mailcatcher', 'Installs a single configurable instance of mailcatcher and installs/configures postfix.'
 
-supports 'rhel'
+supports 'redhat'
 supports 'centos'
 supports 'suse'
 supports 'ubuntu'
